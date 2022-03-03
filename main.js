bdc4c1f9db10f9cda63139f289301305c6221a26
bthol <95544562+bthol@users.noreply.github.com>
2022-03-03 12:57:12 -0800

    style tweak

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const linkEl1 = document.querySelector(`#link-home`);
-const linkEl2 = document.querySelector(`#link-bio`);
-const linkEl3 = document.querySelector(`#link-resume`);
-const linkEl4 = document.querySelector(`#link-projects`);
-const linkEl5 = document.querySelector(`#link-links`);
+// const linkEl1 = document.querySelector(`#link-home`);
+// const linkEl2 = document.querySelector(`#link-bio`);
+// const linkEl3 = document.querySelector(`#link-resume`);
+// const linkEl4 = document.querySelector(`#link-projects`);
+// const linkEl5 = document.querySelector(`#link-links`);
 
-linkEl1.addEventListener('click', errorAlert)
-linkEl2.addEventListener('click', errorAlert)
-linkEl3.addEventListener('click', errorAlert)
-linkEl4.addEventListener('click', errorAlert)
-linkEl5.addEventListener('click', errorAlert)
+// linkEl1.addEventListener('click', errorAlert)
+// linkEl2.addEventListener('click', errorAlert)
+// linkEl3.addEventListener('click', errorAlert)
+// linkEl4.addEventListener('click', errorAlert)
+// linkEl5.addEventListener('click', errorAlert)
 
-function errorAlert() {
-    alert(`That feature is still in development.`);
-};
+// function errorAlert() {
+//     alert(`That feature is still in development.`);
+// };
