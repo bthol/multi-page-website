5f74cd592ab44689646353d6f318560c589dd416
bthol <95544562+bthol@users.noreply.github.com>
2022-03-15 13:50:48 -0700

    aspects of functionality + styling

--- Projects/Projects.js
+++ Projects/Projects.js
@@ -1,5 +1,6 @@
 //////////////////////////GLOABAL VARIABLES/////////////
 let clickCount = 0;
+let downClick = 0;
 let endCount = 0;
 
 ///////////////////////////DATA STRUCTURES//////////////
@@ -7,23 +8,42 @@ const textArr = [`Five`, `Four`, 'Three', `Two`, `One`];
 
 const endMsg = `Done`;
 
+const randText = [`absolutely`, `penguins`, `defenstrate`, `apparatus`, `hot air ballon`, `Nicaragua`, `Stegosauras`, `apples`, `sumset`, `avalanche`];
+
 ///////////////////////////DOM SELECTORS////////////////
 const btn1El = document.querySelector(`#btn1`);
 
 ///////////////////////////EVENT LISTENERS////////////////
 btn1El.addEventListener('click', () => {
-    if (clickCount < textArr.length) {
-        clickCount += 1;
-        btn1El.innerText = textArr[clickCount - 1];
-    } else if (clickCount === textArr.length) {
-        clickCount += 1;
+    clickCount += 1;
+    buttonsClicked();
+    if (downClick < textArr.length) {
+        downClick += 1;
+        btn1El.innerText = textArr[downClick - 1];
+    } else if (downClick === textArr.length) {
+        downClick += 1;
         endCount += 1;
         btn1El.innerText = `${endMsg} ${endCount} times`;
-    } else if (clickCount > textArr.length) {
+    } else if (downClick > textArr.length) {
         btn1El.innerText = `Reset`;
-        clickCount = 0;
+        downClick = 0;
     }
 });
 
-///////////////////////////FUNCTIONS///////////////////////
+function buttonsClicked() {
+    console.log(`button clicked!`);
+    $(`#btnsclick`).text(`Buttons clicked: ${clickCount}`);
+}
+buttonsClicked();
 
+$(`#btnFunct`).on(`click`, () => {
+    clickCount += 1;
+    buttonsClicked();
+    $(`#textFunct`).append(`<p class="rndtxtclass">${randText[Math.floor(Math.random() * randText.length)]}</p>`);
+});
+
+$(`#btnFunctClear`).on(`click`, () => {
+    clickCount += 1;
+    buttonsClicked();
+    $(`.rndtxtclass`).remove();
+});
--- main.js
+++ main.js
@@ -4,19 +4,11 @@
 // const linkEl4 = document.querySelector(`#link-projects`);
 // const linkEl5 = document.querySelector(`#link-links`);
 
-<<<<<<< HEAD
-linkEl1.addEventListener('click', errorAlert)
-linkEl2.addEventListener('click', errorAlert)
-linkEl3.addEventListener('click', errorAlert)
-linkEl4.addEventListener('click', errorAlert)
-linkEl5.addEventListener('click', errorAlert)
-=======
 // linkEl1.addEventListener('click', errorAlert)
 // linkEl2.addEventListener('click', errorAlert)
 // linkEl3.addEventListener('click', errorAlert)
 // linkEl4.addEventListener('click', errorAlert)
 // linkEl5.addEventListener('click', errorAlert)
->>>>>>> new-branch4
 
 // function errorAlert() {
 //     alert(`That feature is still in development.`);
