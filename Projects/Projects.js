9dd5dea8750bd9446f0c150a8b60b876415bc58c
bthol <95544562+bthol@users.noreply.github.com>
2022-03-15 10:03:24 -0700

    more sections for content

--- Projects/Projects.js
+++ Projects/Projects.js
@@ -1,19 +1,27 @@
 //////////////////////////GLOABAL VARIABLES/////////////
 let clickCount = 0;
+let endCount = 0;
 
 ///////////////////////////DATA STRUCTURES//////////////
-const textArr = ['Three', `Two`, `One`];
+const textArr = [`Five`, `Four`, 'Three', `Two`, `One`];
+
+const endMsg = `Done`;
 
 ///////////////////////////DOM SELECTORS////////////////
 const btn1El = document.querySelector(`#btn1`);
 
 ///////////////////////////EVENT LISTENERS////////////////
-btn1El.addEventListener('click', function() {
-    if (clickCount < 3) {
+btn1El.addEventListener('click', () => {
+    if (clickCount < textArr.length) {
         clickCount += 1;
         btn1El.innerText = textArr[clickCount - 1];
-    } else {
-        btn1El.innerText = `Done`;
+    } else if (clickCount === textArr.length) {
+        clickCount += 1;
+        endCount += 1;
+        btn1El.innerText = `${endMsg} ${endCount} times`;
+    } else if (clickCount > textArr.length) {
+        btn1El.innerText = `Reset`;
+        clickCount = 0;
     }
 });
 
